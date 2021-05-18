42a9badb88cbf34dbce8027a38e0486525ea4704
Nicolas Theodoro <nicolastheodoro@gmail.com>
2021-05-18 20:55:34 -0300

    Aula 03 - FrontEnd - Serratec

---
+++ aula03-18052021/tela-login/js/login.js
@@ -0,0 +1,7 @@
+const EMAIL = "admin@admin.com";
+const SENHA = "123";
+
+function logar(){
+    // Aqui tenho que validar se o usuario e senha estão validos.
+    document.window.open("landing-page.html", "_self");
+}
