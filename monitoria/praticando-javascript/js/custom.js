10ba3093321d0ee9c8604c1e313fe57d55e5b63b
Nicolas Theodoro <nicolastheodoro@gmail.com>
2021-05-22 08:43:20 -0300

    Monitoria - Ajustes finos - Serratec

--- monitoria/praticando-javascript/js/custom.js
+++ monitoria/praticando-javascript/js/custom.js
@@ -1,57 +1,52 @@
 // Ligar/Desligar Luz
-(function alternador(){
-    const botaoLigar = document.querySelector("#botao-alternador")
-    let fundoAlternador = document.querySelector(".ligar-desligar")
+(function alternador() {
+  const botaoLigar = document.querySelector("#botao-alternador");
+  let fundoAlternador = document.querySelector(".ligar-desligar");
 
-    botaoLigar.onclick = function(){
-        fundoAlternador.classList.toggle("luz-ligada")
+  botaoLigar.onclick = function () {
+    fundoAlternador.classList.toggle("luz-ligada");
 
-        if(this.innerHTML == "Ligar"){
-            this.innerHTML = "Desligar"
-        } else {
-            this.innerHTML = "Ligar"
-        }
+    if (this.innerHTML == "Ligar") {
+      this.innerHTML = "Desligar";
+    } else {
+      this.innerHTML = "Ligar";
     }
-})()
-
+  };
+})();
 
 // Alterar botao de acordo com o preenchimento do formulário
-function ativaBotao(){
-    let campoNome = document.getElementById("form-nome")
-    let botaoEnviar = document.getElementById("botao-enviar")
+function ativaBotao() {
+  let campoNome = document.getElementById("form-nome");
+  let botaoEnviar = document.getElementById("botao-enviar");
 
-    campoNome.onkeyup = function(){
-        if(campoNome.value.length >= 5){
-            botaoEnviar.classList.remove("desabilitado");
-            botaoEnviar.disabled = false
-        } else {
-            botaoEnviar.classList.add("desabilitado");
-            botaoEnviar.disabled = true
-        }
+  campoNome.onkeyup = function () {
+    if (campoNome.value.length >= 5) {
+      botaoEnviar.classList.remove("desabilitado");
+      botaoEnviar.disabled = false;
+    } else {
+      botaoEnviar.classList.add("desabilitado");
+      botaoEnviar.disabled = true;
     }
+  };
 }
 window.onload = ativaBotao();
-// ATENÇÃO -- Para o window.onload funcionar corretamente, é interessante adicionar o ; ao final para ter uma maior compatibilidade
-
+// ATENÇÃO -- Para o window.onload funcionar corretamente, é interessante adicionar
+// o ; ao final para ter uma maior compatibilidade
 
 // Do a barrel roll
-(function girarTela(){
-    const botaoGirar = document.querySelector("#botao-girar")
-    const telaInteira = document.querySelector("body")
+(function girarTela() {
+  const botaoGirar = document.querySelector("#botao-girar");
+  const telaInteira = document.querySelector("body");
 
-    botaoGirar.onclick = function(){
-        telaInteira.style.transition = "all 1.5s linear"
-        telaInteira.style.transform = "rotate(360deg)"
-        //telaInteira.style.filter = "grayscale(100%)"
-    }
-})()
-
-
+  botaoGirar.onclick = function () {
+    telaInteira.style.transition = "all 1.5s linear";
+    telaInteira.style.transform = "rotate(360deg)";
+    //telaInteira.style.filter = "grayscale(100%)"
+  };
+})();
 
 // Alterar imagem de fundo ao passar com o mouse
-function trocaFundo( imagemfundo ){
-    let fundoModulo = document.querySelector("#fundo-imagens")
-    fundoModulo.style.backgroundImage = "url(../img/"+imagemfundo+")"
+function trocaFundo(imagemfundo) {
+  let fundoModulo = document.querySelector("#fundo-imagens");
+  fundoModulo.style.backgroundImage = "url(../img/" + imagemfundo + ")";
 }
-
-
